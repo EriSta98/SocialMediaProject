a3a7aeeaca16c8dbb52177c208af5ba0b1982d15
EriSta98 <erik.stavenius@gmail.com>
2026-01-23 03:10:15 +0100

    test

--- src/main/java/se/jensen/erik/socialmediaproject/security/SecurityConfig.java
+++ src/main/java/se/jensen/erik/socialmediaproject/security/SecurityConfig.java
@@ -22,7 +22,6 @@ import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
-
 import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.PrivateKey;
@@ -57,13 +56,14 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/users").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
-                        .requestMatchers(HttpMethod.POST, "**/request-token").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/request-token").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
 
-                http.oauth2ResourceServer(oauth2 ->
+                .oauth2ResourceServer(oauth2 ->
                 oauth2.jwt(jwt ->
-                        jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
+                        jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                );
 
         return http.build();
     }
